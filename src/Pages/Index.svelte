2c1f925057bb978629d516090a4201aceb09b60b
ranggahardiansyah81@gmail.com <angga.hardiansyah@rumahzakat.org>
2022-04-27 11:31:07 +0700

    update

---
+++ src/Pages/Index.svelte
@@ -0,0 +1,20 @@
+<script>
+	import DonasiList from "../Components/DonasiList.svelte";
+	import Header from "../Components/Header.svelte";
+	import Promo from "../Components/Promo.svelte";
+	import Welcome from "../Components/Welcome.svelte";
+	import Information from "../Components/Information.svelte";
+    import {donasi} from "../data/donasi.js"
+	
+	let title = "Charity"
+	
+</script>
+
+<!-- <div>
+	<h1>{title}</h1>
+</div> -->
+<Header/>
+<Welcome/>
+<DonasiList {donasi}/>
+<Promo/>
+<Information/>
---
+++ src/data/donasi.js
@@ -0,0 +1,16 @@
+module.exports = {
+    donasi: [
+        {
+            id: 1,
+            title: 'First Donation Project',
+            category: 'Money',
+            thumbnail: 'https://live.staticflickr.com/7721/17102022497_84b7c9c342_w.jpg',
+            pledged: 0,
+            target: 100000,
+            date_end: +new Date('10 August 2022'),
+            profile_photo: '',
+            profile_name: 'Angga Hardiansyah',
+            no_pledges: 0
+        }
+    ]
+}
