181264212baff6d7bfa512a0b5ff5da9093bd45b
ranggahardiansyah81@gmail.com <angga.hardiansyah@rumahzakat.org>
2022-05-27 10:28:06 +0700

    update

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,14 @@
 	import Index from "./Pages/Index.svelte"
 	import About from "./Pages/About.svelte"
 	import Contact from "./Pages/Contact.svelte"
+	import Donation from "./Pages/Donation.svelte"
 
 	let page;
 
 	router('/', () => (page = Index));
 	router('/about', () => (page = About));
 	router('/contact', () => (page = Contact));
+	router('/donation', () => (page = Donation))
 
 	router.start();
 </script>
